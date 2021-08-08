285d6794aeb768e42578967a3700f8c454ad309c
Michelle Swem <swemmichelle990@gmail.com>
2021-08-08 08:25:59 +0100

    Initial commit

---
+++ build/static/js/7.f0a57f5a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[7],{52:function(e,t,c){"use strict";c.r(t);var n=c(1);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:Object(n.jsx)("p",{children:"Page Not Found!"})})}}}]);
+//# sourceMappingURL=7.f0a57f5a.chunk.js.map
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,58 @@
+import { Link, useParams, Route, useRouteMatch } from "react-router-dom";
+import { Fragment, useEffect } from "react";
+
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
+const QuoteDetail = () => {
+  const params = useParams();
+  const { quoteId } = params;
+  const match = useRouteMatch();
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className = 'centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className = 'centered'>{error}</p>
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No Quote Found!</p>;
+  }
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
